fe0963e622ad5d469422b5d36e35b5c360cf58d8
Ada Lau <adalau@sino.com>
2024-11-01 10:25:03 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
-	images: {
-		loader: "akamai",
-		path: "",
-	},
-	assetPrefix: "./",
+	basePath: "/my-profile",
+	reactStrictMode: true,
 };
 
 export default nextConfig;
