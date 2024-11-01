3b194c3d65d15e9b557da121f9fa16f280264abe
Ada Lau <adalau@sino.com>
2024-11-01 12:10:01 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,6 @@ const nextConfig = {
 	output: "export",
 	basePath: "/my-profile",
 	reactStrictMode: true,
-	images: {
-		localPatterns: [
-			{
-				pathname: "/images/**",
-				search: "",
-			},
-			{
-				pathname: "/projects/**",
-				search: "",
-			},
-		],
-	},
 };
 
 export default nextConfig;
