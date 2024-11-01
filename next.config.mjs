4d117a235065e29603c4ba95c050bbfbc1c69179
Ada Lau <adalau@sino.com>
2024-11-01 16:48:39 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-	output: "export",
-	basePath: "/my-profile",
+	// output: "export",
+	// assetPrefix: "/",
+	// basePath: "/my-profile",
+	// reactStrictMode: false,
 };
 
 export default nextConfig;
